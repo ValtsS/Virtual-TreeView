6db987c0afc699dceac79bee58f243f4ab6d4b9b
joachim.marder@gmail.com <joachim.marder@gmail.com@38df5cc4-5811-11de-992f-fdd7e25b9163>
2013-05-17 18:46:48 +0000

    Fixed #337: Cannot "grab" item for dragging, odd behaviour of multi selecting with the selection rectangle when toFullRowSelect is True but toSimpleDrawSelection is False.  (thx to Stefan Glienke) # Conflicts: #	CHANGES.txt #	Source/VirtualTrees.pas

--- Source/VirtualTrees.pas
+++ Source/VirtualTrees.pas
@@ -14105,7 +14105,7 @@ begin
 
       // Simple selection allows to draw the selection rectangle anywhere. No intersection with node captions is
       // required. Only top and bottom bounds of the rectangle matter.
-      if SimpleSelection then
+      if SimpleSelection or (toFullRowSelect in FOptions.FSelectionOptions) then
       begin
         IsInOldRect := (NextTop > OldRect.Top) and (CurrentTop < OldRect.Bottom) and
           ((FHeader.Columns.Count = 0) or (FHeader.Columns.TotalWidth > OldRect.Left)) and (NodeLeft < OldRect.Right);
@@ -23477,7 +23477,7 @@ begin
 
     // Query the application to learn if dragging may start now (if set to dmManual).
     if Assigned(HitInfo.HitNode) and not AutoDrag and (DragMode = dmManual) then
-      AutoDrag := DoBeforeDrag(HitInfo.HitNode, Column) and (IsCellHit or FullRowDrag);
+      AutoDrag := DoBeforeDrag(HitInfo.HitNode, Column) and (IsAnyHit or FullRowDrag);
 
     // handle node height tracking
     if IsHeightTracking then
