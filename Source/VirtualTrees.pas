7c6fe237cf0e8a445263cfdd96b4ed1f166359d0
joachim.marder@gmail.com <joachim.marder@gmail.com@38df5cc4-5811-11de-992f-fdd7e25b9163>
2013-07-11 15:18:30 +0000

    Fixed missing hhiOnCheckbox when double click on a checkbox in the header.

--- Source/VirtualTrees.pas
+++ Source/VirtualTrees.pas
@@ -12233,6 +12233,8 @@ begin
                   // WM_NCLBUTTONDBLCLK
                   Button := mbLeft;
               end;
+              if Button = mbLeft then
+                Columns.AdjustDownColumn(P);
               FColumns.HandleClick(P, Button, True, True);
             end;
       end;
