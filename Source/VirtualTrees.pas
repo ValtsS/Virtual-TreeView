4e2d3ca32db5ca468bf3cd9fa47eb87752fcdc86
obones <obones@free.fr>
2015-07-08 11:23:34 +0200

    When using checkboxes in the column header, the custom check image list must be used as needed to avoid access violations.
    # Conflicts:
    #	Source/VirtualTrees.pas

--- Source/VirtualTrees.pas
+++ Source/VirtualTrees.pas
@@ -11118,6 +11118,8 @@ var
             with Header.Treeview do
             begin
               ColImageInfo.Images := GetCheckImageListFor(CheckImageKind);
+              if not Assigned(ColImageInfo.Images) then
+                ColImageInfo.Images := CustomCheckImages;
               ColImageInfo.Index := GetCheckImage(nil, FCheckType, FCheckState, IsEnabled);
               ColImageInfo.XPos := GlyphPos.X;
               ColImageInfo.YPos := GlyphPos.Y;
