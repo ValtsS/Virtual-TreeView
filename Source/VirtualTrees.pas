b9c0edae58cbd49c224af48523c5a72bac114fb3
Joachim Marder <joachim.marder@gmail.com>
2015-07-27 15:12:27 +0200

    TBaseVirtualTree.EnsureNodeFocused() should prefer a selected node, if there is any, instead of always using the first visible node.
    # Conflicts:
    #	Source/VirtualTrees.pas

--- Source/VirtualTrees.pas
+++ Source/VirtualTrees.pas
@@ -22854,6 +22854,8 @@ end;
 procedure TBaseVirtualTree.EnsureNodeFocused();
 begin
   if FocusedNode = nil then
+    FocusedNode := Self.GetFirstSelected();
+  if FocusedNode = nil then
     FocusedNode := Self.GetFirstVisible();
 end;
 
