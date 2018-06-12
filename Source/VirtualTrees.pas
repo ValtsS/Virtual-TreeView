8473859be1cd568822ffad45bd7e3b53f70a29b6
Joachim Marder <joachim.marder@gmail.com>
2018-06-12 13:53:28 +0200

    Fixed an incorrect calculation of the string edit link height. The new calculation is more precise and allows for smaller heights while making sure that the caret does not disappear (a windows bug).
    # Conflicts:
    #	Source/VirtualTrees.pas

--- Source/VirtualTrees.pas
+++ Source/VirtualTrees.pas
@@ -7107,7 +7107,8 @@ begin
   else
   begin
     Result := FOwner.DoCreateDataObject;
-    if Result = nil then
+    if (Result = nil) and not Assigned(FOwner.OnCreateDataObject) then
+      // Do not create a TVTDataObject if the event handler explicitely decided not to supply one, issue #736.
       Result := TVTDataObject.Create(FOwner, False) as IDataObject;
   end;
 end;
